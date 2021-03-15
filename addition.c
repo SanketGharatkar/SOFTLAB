55e48d50845b21cf25d46fbe491f24dff6d38a46
Sanket <sanketgharatkar123@gmail.com>
2021-03-15 12:34:51 +0530

    printfs are added

--- addition.c
+++ addition.c
@@ -11,6 +11,16 @@ int main()
 
     printf("Sum : %d\n", sum);
     printf("Shanky");
+printf("Shanky");
+
+printf("Shanky");
+
+printf("Shanky");
+
+printf("Shanky");
+
+printf("Shanky");
+
 
     return (0);
 }
